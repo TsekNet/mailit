0c1325159d14532069af0f82e3a32d24e42d96d4
tseknet <dan@tseknet.com>
2019-05-21 09:51:33 -0400

    Limit to 3 top image posts Reformat html Add better error variables

--- web/reddit.go
+++ web/reddit.go
@@ -1,14 +1,15 @@
-package reddit
+﻿package reddit
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
 
 // Listing consists the initial reddit post object
 type Listing struct {
-	Kind     string `json:"kind"`
 	MetaData struct {
 		Modhash string         `json:"modhash"`
 		Dist    int            `json:"dist"`
@@ -18,17 +19,17 @@ type Listing struct {
 
 // PostMetaData consists of specific meta data and posts
 type PostMetaData struct {
-	Kind string `json:"kind"`
 	Post struct {
 		Title string `json:"title"`
 		Link  string `json:"url"`
+		Hint  string `json:"post_hint"`
 	} `json:"data"`
 }
 
 // GetTopPosts return the 20 top posts of /r/<subreddit>
 func GetTopPosts() ([]PostMetaData, error) {
 	url := "https://www.reddit.com/r/aww/.json?"
-	client := http.Client{
+	redditClient := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
 
@@ -36,17 +37,37 @@ func GetTopPosts() ([]PostMetaData, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("User-Agent", "reddit-post")
 
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
+	req.Header.Set("User-Agent", "reddit-posts")
+
+	res, getErr := redditClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
 	}
 
-	data := Listing{}
-	if err = json.NewDecoder(res.Body).Decode(&data); err != nil {
-		return nil, err
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
 	}
+
+	var data Listing
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	var post []PostMetaData
+
+	for _, e := range data.MetaData.Posts {
+		if e.Post.Hint == "image" {
+			post = append(post, e)
+		}
+		if len(post) == 3 {
+			break
+		}
+	}
+
+	data.MetaData.Posts = post
 
 	return data.MetaData.Posts, nil
 }
